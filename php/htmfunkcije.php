9f05a5cea809210dbec6b7c48872aa3278da90bc
Jan Merhar <merharjan@gmail.com>
2020-01-01 22:17:28 +0100

    Dodal stran v PHP 2

--- php/htmfunkcije.php
+++ php/htmfunkcije.php
@@ -13,10 +13,21 @@
             <script src="../js/form.js"></script>
             <?php
         }
+        else if($status == 0)
+        {
+            ?>
+            <link href="css/login.css" rel="stylesheet" type="text/css" /> <!-- mogoče naredim posebno funkcijo za login/register -->
+            <link href="css/nav.css" rel="stylesheet" type="text/css" />
+        
+            <link rel="icon" href="images/favicon.png" />
+
+            <script src="js/form.js"></script>
+            <?php
+        }
         else
         {
             ?>
-            <link href="css/login.css" rel="stylesheet" type="text/css" /> <!-- moggoče naredim posebno funkcijo za login/register -->
+            <link href="css/login.css" rel="stylesheet" type="text/css" /> <!-- mogoče naredim posebno funkcijo za login/register -->
             <link href="css/nav.css" rel="stylesheet" type="text/css" />
         
             <link rel="icon" href="images/favicon.png" />
@@ -26,7 +37,12 @@
         }
     }
 
-    function navbar($status = 1)
+    /*
+        0 => za datoteke iz root-a
+        1 => za datoteke v mapi prve stopnje
+        "" => za login in register strani
+    */
+    function navbar($status = 1, $jsfunkcija = "")
     {
         session_start();
         ?>
@@ -41,7 +57,7 @@
             <title>Learn</title>
         </head>
 
-        <body onload="mainFunction()">
+        <body onload="<?php echo $jsfunkcija ?>">
             <header>
             <a href="#"><img src="../images/logo.png" alt="logo"/></a>
             <nav>
@@ -85,13 +101,97 @@
         }        
     }
 
-    function body()
+    function leva_skatla()
     {
         ?>
-
+        <div class="leva_skatla">
+          Uporabnik
+          <ul>
+            <br/>
+            <li><a href="#">Testi in ocene/zgodovina</a></li>
+            <li><a href="#">Izpis iz učilnice</a></li>
+          </ul>
+        </div>
+        <?php
     }
-    
-    navbar();
+
+    function levo($status = 0)    
+    {
+        ?>
+        <div class="ogrodje">
+            <div class="levo">
+
+        <?php
+        if($status != 0)
+            leva_skatla()
+        ?>
+            </div>
+        <?php
+    }
+
+    function glava($besedilo = "Načrtovanje in postavljanje podatkovnih baz")
+    {
+        ?>
+        <div class="vsebina">
+            <div class="glava">
+        <?php
+            echo $besedilo.'<br/>';
+        ?>
+            </div>
+        <?php
+    }
+
+    function vnos_podatkov()
+    {
+        ?>
+        <div class="vnos_podatkov">
+         <div id="formdiv">
+
+         </div>
+       </div>
+       <?php
+    }
+
+    function desna_skatla()
+    {
+        ?>
+        <div class="desna_skatla">Skrbnik
+          <ul>
+            <br/>
+            <li><a href="#">Ustvari test</a></li>
+            <li><a href="#">Pregled ocen in testov</a></li>
+            <li><a href="#">Pregled uporabnik in izbris</a></li>
+          </ul>
+        </div>
+        <?php
+    }
+
+    function desno($status = 0)
+    {
+        ?>
+        <!-- zaključim DIV od .ogrodje -->
+        </div>
+
+        <div class="desno">
+        <?php
+        if($status != 0)
+            desna_skatla()
+        ?>
+        </div>
+
+        <!-- zaključek samega dokumenta HTML --> 
+        </body>
+        </html>
+        <?php
+    }
+    navbar(1, "mainFunction()");
+    levo(1);
+    glava("Tvoja mater ima gej");
+    /*
+        vsebina same spletne strani
+    */
+    vnos_podatkov();
+    desno(1);
     
 
 ?>
