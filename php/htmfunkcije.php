d917738a6155067b9bdf0d4112e3463e2b6d62c5
Jan Merhar <merharjan@gmail.com>
2020-07-21 14:21:47 +0200

    nov navbar za implementacijo

--- php/htmfunkcije.php
+++ php/htmfunkcije.php
@@ -32,10 +32,13 @@
         <html>
         <head>
             <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 
             <?php metaHead($status) ?>
+            <!--
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
+            -->
             <title>Neo learn</title>
         </head>
 
@@ -196,6 +199,7 @@
         <?php
     }
 
+    // tudi za header
     function desno($status = 0)
     {
         ?>
@@ -207,6 +211,15 @@
             desna_skatla()
         ?>
         </div>
+
+        <!--
+             zakljucim div.ogrodje 
+        -->
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
         </body>
         </html>
