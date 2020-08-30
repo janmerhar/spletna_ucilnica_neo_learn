4533a4bd85e687bcc1a98d7823038881f2478fcb
Jan Merhar <merharjan@gmail.com>
2020-08-30 23:49:23 +0200

    dodal odstranjevanje elementov iz učilnice brez axiosa

--- src/views/Ucilnica.vue
+++ src/views/Ucilnica.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <app-glava>{{ ucilnica }}</app-glava>
-    <app-sklop-vsebina v-for="sklop in sklopi" :sklop="sklop" :key="sklop.sklop_id"></app-sklop-vsebina>
+    <div class="vsebina_sklopa" v-for="sklop in sklopi" :key="sklop.sklop_id">
+      <p>
+        {{ sklop.ime_sklopa }}
+        <button class="gumb-small" @click="removeElement(sklop.id_sklopa)">-</button>
+      </p>
+      <!-- dodaj @click za brisanje sklopa -->
+      <ul>
+        <li v-for="list in sklop.vsebina" :key="sklop.id_sklopa + '.' + list.id_vsebine">
+          {{ list.besedilo }}
+          <button
+            class="gumb-small"
+            @click="removeElement(sklop.id_sklopa, list.id_vsebine)"
+          >-</button>
+          <!-- dodaj @click za brisanje vsebine -->
+          <!-- preveri tip podatka in ugotovi, ali boš dal link, text ali sliko => method() -->
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import Glava from '../components/layout/Glava.vue'
-import SklopVsebina from '../components/ucilnica/SklopVsebina.vue'
+
     export default {
         // dodaj še Vuex za preverjanje logina in skrbnika
       data() {
@@ -23,19 +40,45 @@ import SklopVsebina from '../components/ucilnica/SklopVsebina.vue'
       },
       components: {
         appGlava: Glava,
-        appSklopVsebina: SklopVsebina,
       },
       methods: {
         removeElement(id_sklopa, id_vsebine = false) {
           let sendData = {
             id_sklopa
           }
-          if(id_vsebine !== false)
+
+          if(id_vsebine !== false) 
             sendData.id_vsebine = id_vsebine
           
+          /*
           axios.post("ucilnice/vsebina/vsebinaremove.php", sendData)
           .then(response => console.log(response))
           .catch(err => console.log(err))
+          */
+
+          if(id_vsebine !== false) {
+           for(const [index, sklop] of this.sklopi.entries()) {
+              if(sklop.id_sklopa == id_sklopa) {
+                for(const [index2, vsebina] of this.sklopi[index].vsebina.entries()) {
+                  if(vsebina.id_vsebine == id_vsebine) {
+                    console.log(vsebina.id_vsebine)
+                    this.sklopi[index].vsebina.splice(index2, 1)
+                  }
+                }
+              }
+            }
+          }
+          else {
+            for(const [index, sklop] of this.sklopi.entries()) {
+              if(sklop.id_sklopa == id_sklopa) {
+                console.log(index)
+                this.sklopi.splice(index, 1)
+                break
+              }
+            }
+          }
+          
+          
         }
       },
       created() {
@@ -49,6 +92,7 @@ import SklopVsebina from '../components/ucilnica/SklopVsebina.vue'
           this.sklopi = response.data
         })
         .catch(error => console.log(error))
+        
       }
     }
 </script>
