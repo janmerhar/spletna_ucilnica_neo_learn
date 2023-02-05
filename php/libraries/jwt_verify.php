07d2b147b2bb24188973e1e9ee7521aee11bbf27
Jan Merhar <merharjan@gmail.com>
2023-02-05 02:51:20 +0100

    Add getAuthorizationHeader() in libraries/jwt_verify.php

--- php/libraries/jwt_verify.php
+++ php/libraries/jwt_verify.php
@@ -1,7 +1,27 @@
 <?php
     require_once 'jwt.php';
-    // preverim, ali je poslan token oz. če je NULL
-    if(isset($json_data['token']))
+
+    function getAuthorizationHeader(){
+        $headers = null;
+
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER["Authorization"]);
+        }
+        else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { 
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+        } elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            
+            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+            
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+
+        return $headers;
+    }
+
     {
         // ustvarim objekt
         // preverim, ali je token veljaven
