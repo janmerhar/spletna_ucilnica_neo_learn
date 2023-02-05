f01722348780973759e1cc9d5e1f8b018af14ff3
Jan Merhar <merharjan@gmail.com>
2023-02-06 00:21:42 +0100

    Change default values of store and do a format in store/store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,55 +1,57 @@
-import Vue from 'vue'
-import axios from 'axios'
-import Vuex from 'vuex'
+import Vue from "vue"
+// import axios from "axios"
+import Vuex from "vuex"
 import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-        token: null,
-        username: 'merjan',
-        isLogin: true,
-        ucilnica: '',
-        isSkrbnik: false
+  state: {
+    token: "",
+    username: "",
+    isLogin: false,
+    ucilnica: "",
+    isSkrbnik: false,
+  },
+  mutations: {
+    setUsername: (state, username) => {
+      state.username = username
     },
-    mutations: {
-        setUsername: (state, username) => {
-            state.username = username
-        },
-        setToken: (state, token) => {
-            state.token = token
-        },
-        setLogin: (state, isLogin) => {
-            state.isLogin = isLogin
-        },
-        setUcilnica: (state, ucilnica) => {
-            state.ucilnica = ucilnica
-        },
-        setSkrbnik: (state, isSkrbnik) => {
-            state.isSkrbnik = isSkrbnik
-        }
+    setToken: (state, token) => {
+      state.token = token
     },
-    getters: {
-        getUsername: state => state.username,
-        getLogin: state => state.isLogin,
-        getToken: state => state.token,
-        getUcilnica: state => state.ucilnica,
-        getSkrbnik: state => state.isSkrbnik,
+    setLogin: (state, isLogin) => {
+      state.isLogin = isLogin
     },
-    actions: {
-        async checkLogin({ commit, state, }) {
-            await axios.post("libraries/beforeEach.php", {
-                token: state.token
-            })
-                .then((res) => {
-                    console.log(res.data)
-                    if (res.data.token)
-                        commit("setToken", res.token)
-                })
-        }
+    setUcilnica: (state, ucilnica) => {
+      state.ucilnica = ucilnica
     },
-    plugins: [createPersistedState({
-        paths: ['username', 'isLogin', 'ucilnica', 'isSkrbnik']
-    })],
+    setSkrbnik: (state, isSkrbnik) => {
+      state.isSkrbnik = isSkrbnik
+    },
+  },
+  getters: {
+    getUsername: (state) => state.username,
+    getLogin: (state) => state.isLogin,
+    getToken: (state) => state.token,
+    getUcilnica: (state) => state.ucilnica,
+    getSkrbnik: (state) => state.isSkrbnik,
+  },
+  actions: {
+    // async checkLogin({ commit, state }) {
+    //   await axios
+    //     .post("libraries/beforeEach.php", {
+    //       token: state.token,
+    //     })
+    //     .then((res) => {
+    //       console.log(res.data)
+    //       if (res.data.token) commit("setToken", res.token)
+    //     })
+    // },
+  },
+  plugins: [
+    createPersistedState({
+      paths: ["token", "username", "isLogin", "ucilnica", "isSkrbnik"],
+    }),
+  ],
 })
