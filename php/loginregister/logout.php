5d301970e038ae5e224d9e31151483581d04c6cc
Jan Merhar <merharjan@gmail.com>
2023-02-06 00:20:27 +0100

    Fix cookie not deleting in loginregister/logout.php

--- php/loginregister/logout.php
+++ php/loginregister/logout.php
@@ -4,6 +4,6 @@
     
     $response['status'] = true;
     if(isset($_COOKIE['refresh_token']))
-        setcookie("refresh_token", "", time() - 60, NULL, NULL, false, true);
+        setcookie("refresh_token", "", time() - 60 * 24 * 60 * 60, "/", NULL, false, true);
 
     echo json_encode($response);
