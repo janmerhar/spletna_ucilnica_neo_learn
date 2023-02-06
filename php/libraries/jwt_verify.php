39400517402a4572d9d21fd37014ee246dd3c959
Jan Merhar <merharjan@gmail.com>
2023-02-07 00:34:11 +0100

    Remove unneeded if statement in libraries/jwt_verify.php

--- php/libraries/jwt_verify.php
+++ php/libraries/jwt_verify.php
@@ -40,15 +40,6 @@
         $token = new Token($bearer_token, "token");
         $response['token'] = $token->getToken();
     }
-    // če ne dobim tokena, ga moram prevzeti iz piškotka, v primeru, da obstaja
-    else if(isset($_COOKIE['refresh_token']))
-    {
-        // piškotek
-        $token = new Token($_COOKIE['refresh_token'], "cookie");
-        $response['token'] = $token->getToken();
-    }
-    // če pa to sedaj ni nič delovalo, pa samo zavrnem transakcijo
-    // http_response_code(403); => ACCESS DENIED
     else 
     {
         // http_response_code(403);
