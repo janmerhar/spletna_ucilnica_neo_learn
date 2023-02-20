c13c9c6fca875962cfe6fe7de44706481d8d13d7
Jan Merhar <merharjan@gmail.com>
2023-02-20 12:38:20 +0100

    Add send e-mail implementation in loginregister/sendmail.com

--- php/loginregister/sendmail.php
+++ php/loginregister/sendmail.php
@@ -10,33 +10,29 @@
     $mail = new PHPMailer(true);
 
     try {
-        //Server settings
-        $mail->CharSet = "utf-8";
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.mail.com';                        // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'learn.ucilnica@mail.com';                     // SMTP username
-        $mail->Password   = 'learn.ucilnica';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->IsSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'spletna.prodajalna.ep@gmail.com';
+        $mail->Password = 'mjythpndfffbxlke';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
 
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );
+        // Sumniki fix
+        $mail->CharSet = 'UTF-8';
 
         //Recipients
-        $mail->setFrom('learn.ucilnica@mail.com', 'Learn Ucilnica');
+        $mail->setFrom('spletna.prodajalna.ep@gmail.com', 'Learn Ucilnica');
         $mail->addAddress($json_data['email']);               // Name is optional
+
+        $vkey = md5(time() . $json_data['username']);
+        $url ="localhost/spletna_ucilnica_neo_learn/php/loginregister/verify_account.php?vkey=$vkey";
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = 'Potrdite svoj uporabniški račun';
         $mail->Body    = '<p>Spoštovani!</p>Hvala za registracijo pri spletni učilnici Learn. Pred prvo prijavo morate potrditi svoj 
-        e-poštni naslov s klikom na povezavo';
+        e-poštni naslov s klikom na povezavo ' . "<a href='$url'>$url</a>";
 
         $mail->send();
         // zgeneriram vkey in ga potem pošljem
@@ -45,7 +41,7 @@
             UPDATE uporabnik
             SET vkey = ?
             WHERE upime = ?
-        ", [md5(time() . $json_data['username']), $json_data['username']]);
+        ", [ $vkey, $json_data['username'] ]);
     } catch (Exception $e) {
         $response['status'] = false;
     }
