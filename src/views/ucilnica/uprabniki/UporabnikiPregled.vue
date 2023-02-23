80c0a46f5c40b0f772906ca00d91b506262fc76c
Jan Merhar <merharjan@gmail.com>
2023-02-23 19:07:37 +0100

    Fix outdated method for getting ucilnica in uprabniki/UporabnikiPregled.vue and a reformat done

--- src/views/ucilnica/uprabniki/UporabnikiPregled.vue
+++ src/views/ucilnica/uprabniki/UporabnikiPregled.vue
@@ -1,60 +1,63 @@
 <template>
   <div>
     <app-glava>Pregled uporabnikov</app-glava>
-    <app-tabela :tabela="uporabniki" @izbris="izbris">Ni včlanjenih uporabnikov</app-tabela>
+    <app-tabela :tabela="uporabniki" @izbris="izbris"
+      >Ni včlanjenih uporabnikov</app-tabela
+    >
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Glava from '../../../components/layout/Glava.vue'
-import Tabela from '../../../components/ucilnica/Tabela.vue'
+import axios from "axios"
+import Glava from "../../../components/layout/Glava.vue"
+import Tabela from "../../../components/ucilnica/Tabela.vue"
 
-    export default {
-        data() {
-            return {
-                uporabniki: {} 
-            }
-        },
-        methods: {
-            izbris(event) {
-                console.log(event)
-                axios.post("uporabnik/clanstvo.php", {
-                    type: 'izbris',
-                    username: event,
-                    ucilnica: this.$store.getters.getUcilnica
-                })
-                .then(res => {
-                    console.log(res.data)
-                    if(res.data.status == true) {
-                        axios.post("uporabnik/vclanjeniuporabniki.php", {
-                            ucilnica: this.$store.getters.getUcilnica
-                        })
-                        .then(res => {
-                            if(res.data.status === true) {
-                                this.uporabniki = res.data.tabela
-                            }
-                        })
-                    }
-                })
-            }
-        },
-        mounted() {
-            axios.post("uporabnik/vclanjeniuporabniki.php", {
-                ucilnica: this.$store.getters.getUcilnica
-            })
-            .then(res => {
-                if(res.data.status === true) {
-                    this.uporabniki = res.data.tabela
-                }
-            })
-        },
-        components: {
-            appTabela: Tabela,
-            appGlava: Glava
-        }
+export default {
+  data() {
+    return {
+      uporabniki: {},
     }
+  },
+  methods: {
+    izbris(event) {
+      axios
+        .post("uporabnik/clanstvo.php", {
+          type: "izbris",
+          username: event,
+          ucilnica: this.$route.params.ucilnica,
+        })
+        .then((res) => {
+          console.log(res.data)
+          if (res.data.status == true) {
+            axios
+              .post("uporabnik/vclanjeniuporabniki.php", {
+                ucilnica: this.$route.params.ucilnica,
+              })
+              .then((res) => {
+                if (res.data.status === true) {
+                  this.uporabniki = res.data.tabela
+                }
+              })
+          }
+        })
+    },
+  },
+  mounted() {
+    axios
+      .post("uporabnik/vclanjeniuporabniki.php", {
+        ucilnica: this.$route.params.ucilnica,
+      })
+      .then((res) => {
+        if (res.data.status === true) {
+          this.uporabniki = res.data.tabela
+        }
+      })
+  },
+  components: {
+    appTabela: Tabela,
+    appGlava: Glava,
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
