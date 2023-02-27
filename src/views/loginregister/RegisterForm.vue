49e5150245e3366266779f8ef0e40b9024e44bdd
Jan Merhar <merharjan@gmail.com>
2023-02-27 05:33:08 +0100

    Fix modal tag in loginregister/RegisterForm.vue

--- src/views/loginregister/RegisterForm.vue
+++ src/views/loginregister/RegisterForm.vue
@@ -77,7 +77,7 @@
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog" role="document" id="napaka">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Opozorilo</h5>
